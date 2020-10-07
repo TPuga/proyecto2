d42e9995df34440418296e32c86de0aebcdc3917
Tony <72223126+TPuga@users.noreply.github.com>
2020-10-07 11:51:47 +0200

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+num1 = int(input("dime un numero"))
+num2 = int(input("dime otro numero")) 
+suma = num1 + num2 /  2
+nom = "hola mundo"
+print nom
+print num1
+print num2
+print suma
